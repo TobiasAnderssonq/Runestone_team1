80b6f4679260a50a7fa11ec737f8d1eb39b7b5a6
Alexander Lind <Alexander.E.Lind@gmail.com>
2017-05-05 12:09:14 +0200

    Created a test file

---
+++ HelloWrold.java
@@ -0,0 +1,27 @@
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.Motor;
+import lejos.utility.Delay;
+
+/**
+ * This is a basic leJOS program
+ * @author Shea Bunge
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * Main method
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Motor.A.setSpeed(720);// 2 RPM
+		Motor.C.setSpeed(720);
+		Motor.A.forward();
+		Motor.C.forward();
+		Delay.msDelay(1000);
+		Motor.A.stop();
+		Motor.C.stop();
+		
+	}
+}
