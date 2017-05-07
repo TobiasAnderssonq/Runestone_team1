ede51631b01457a0ed175ae663e777749bf542aa
TobiasAnderssonq <anderssontobias93@gmail.com>
2017-05-07 17:21:35 +0200

    Added documentation to the Database.

--- Database.java
+++ Database.java
@@ -2,14 +2,20 @@ import java.util.ArrayList;
 
 public class Database {
 	
-	private ArrayList<Shelf> list = new ArrayList<Shelf>();
+	private ArrayList<Shelf> list = new ArrayList<Shelf>(); //The list 
 	
-	
+	/**
+	 * Returns the full database ArrayList
+	 * @return  The full database ArrayList.
+	 */
 	public ArrayList<Shelf> getListOfShelves() {
 		return this.list;
-		
 	}
 	
+	/**
+	 * Stores a Shelf object in the database where the coordinates match. 
+	 * @param  newShelf  The Shelf to be stored in the database
+	 */
 	public void storePackage(Shelf newShelf) {
 		for(int i = 0; i < list.size(); i++) {
 			if(Coordinates.compareCoordinates(newShelf.getCoordinates(), list.get(i).getCoordinates())) {
@@ -18,6 +24,11 @@ public class Database {
 		}
 	}
 	
+	/**
+	 * Finds the first Shelf object in the database which has no Package object and returns
+	 * the coordinates of that Shelf object. 
+	 * @return	The coordinates of the first empty slot.
+	 */
 	public Coordinates findEmptySlot() {
 		for(int i = 0; i < list.size(); i++) {
 			if(list.get(i).getPackage() == null) {
@@ -27,21 +38,40 @@ public class Database {
 		return null;
 	}
 	
+	/**
+	 * Finds the first Shelf object in the database which contains a Package object with 
+	 * the name defined by packageName and returns the coordinates of that Shelf.
+	 * @param	packageName	The name of the package to be found in the database
+	 * @return	The coordinates in the Shelf object.
+	 */
 	public Coordinates getPackageLocation(String packageName) {
 		return null;
 		//TOOD
 	}
 	
+	/**
+	 * Finds the first Shelf object in the database which contains a Package object with 
+	 * the ID defined by packageID and returns the coordinates of that Shelf.
+	 * @param	packageID	The ID of the package to be found in the database
+	 * @return	The coordinates in the Shelf object.
+	 */
 	public Coordinates getPackageLocation(int packageID) {
 		return null;
 		//TODO
 	}
 	
-	public void populateDatabase(int rows, int shelvesPerRow, int shelfHeight) {
+	
+	/**
+	 * Populates the database with a set of empty shelves.
+	 * @param	rows			The number of rows in the warehouse.
+	 * @param	shelvesPerRow	The number shelves per row in the warehouse.
+	 * @param	shelfLevels		The number of levels per shelf in the warehouse.
+	 */
+	public void populateDatabase(int rows, int shelvesPerRow, int shelfLevels) {
 		
 		for(int i = 0; i < rows; i++) {
 			for(int k = 0; k < shelvesPerRow; k++) {
-				for(int j = 0; j < shelfHeight; j++) {
+				for(int j = 0; j < shelfLevels; j++) {
 					Coordinates coord = new Coordinates(i,k,j);
 					Shelf newShelf = new Shelf(null,coord);
 					list.add(newShelf);
@@ -52,6 +82,9 @@ public class Database {
 		
 	}
 	
+	/**
+	 * Prints the current database in the form "Package - Coordinates"
+	 */
 	public void printDB() {
 		for(int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i).getPackage() + " " + list.get(i).getCoordinates().x + 
--- Server.java
+++ Server.java
@@ -2,10 +2,15 @@
 public class Server {
 	public static void main(String [ ] args) {
 		
+		
+		
+		
+		
+		//Test code for the database
 		Database db = new Database(); 
 		db.populateDatabase(2, 6, 3);
 		db.printDB();
-		Package pack = new Package(12345,"nyttpaket");
+		Package pack = new Package(12345,"newPackage");
 		Coordinates coord = new Coordinates(1,2,2);
 		Shelf newShelf = new Shelf(pack, coord);
 		db.storePackage(newShelf);
